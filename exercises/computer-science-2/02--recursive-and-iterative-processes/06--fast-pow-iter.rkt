1d9010ad79a0c618df80c78ac610a7c5104269c7
Deian Goranov <deian.goranov@gmail.com>
2019-10-14 12:52:03 +0300

    Rename function to fast-pow-iter

--- exercises/computer-science-2/02--recursive-and-iterative-processes/06--fast-pow-iter.rkt
+++ exercises/computer-science-2/02--recursive-and-iterative-processes/06--fast-pow-iter.rkt
@@ -5,12 +5,12 @@
 ; ### Задача 6
 ; Напишете итеративен вариант на `fast-pow` от миналото упражнение
 
-(define (fast-pow x n) void)
+(define (fast-pow-iter x n) void)
 
 (run-tests (test-suite "count-digits tests"
-                       (check-eq? (fast-pow 10 4) 10000)
-                       (check-eq? (fast-pow 5 3) 125)
-                       (check-eq? (fast-pow -5 3) -125)
-                       (check-eq? (fast-pow -5 0) 1))
+                       (check-eq? (fast-pow-iter 10 4) 10000)
+                       (check-eq? (fast-pow-iter 5 3) 125)
+                       (check-eq? (fast-pow-iter -5 3) -125)
+                       (check-eq? (fast-pow-iter -5 0) 1))
            'verbose)
 
