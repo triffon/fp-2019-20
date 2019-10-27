06a1ac4accb63604047d2876eccfe23f1d5b105d
Georgi Lyubenov <godzbanebane@gmail.com>
2019-10-27 20:54:42 +0200

    Add k-ary skeleton + tests

---
+++ exercises/lab/homework/00/solutions/k-ary.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(provide from-k-ary
+         to-k-ary)
+
+(define (from-k-ary n k) void)
+
+(define (to-k-ary n k) void)
---
+++ exercises/lab/homework/00/tests/k-ary-tests.rkt
@@ -0,0 +1,55 @@
+#lang racket
+
+(require quickcheck
+         rackunit
+         rackunit/quickcheck)
+
+(require (prefix-in solutions. "../solutions/k-ary.rkt"))
+
+
+(define from-to-id
+  (property ((n (choose-integer 1 2147483561))
+             (k (choose-integer 2 9)))
+    (= n (solutions.from-k-ary (solutions.to-k-ary n k) k))))
+
+(define (list-to-num xs)
+  (foldl (lambda (x acc)
+           (+ (* 10 acc) x))
+         0
+         xs))
+
+(define to-from-id
+  (property ((k (choose-integer 2 9)))
+    (property ((ns (sized
+                     (lambda (n) (choose-list (choose-integer 0 (- k 1)) n)))))
+      (let ((m (list-to-num ns)))
+        (= m (solutions.to-k-ary (solutions.from-k-ary m k) k))))))
+
+(test-case
+  "PROPERTY: Converting to k-ary and then from k-ary is the identity"
+  (check-property from-to-id))
+
+(test-case
+  "PROPERTY: Converting from k-ary and then to k-ary is the identity"
+  (check-property to-from-id))
+
+(test-case
+  "Unit tests: to-k-ary"
+  (test-begin
+    (check-equal? (solutions.to-k-ary 2 5) 2)
+    (check-equal? (solutions.to-k-ary 6 7) 6)
+    (check-equal? (solutions.to-k-ary 0 9) 0)
+    (check-equal? (solutions.to-k-ary 1234567123 6) 322301020311)
+    (check-equal? (solutions.to-k-ary 83742878901 5) 2333001134111101)
+    (check-equal? (solutions.to-k-ary 1438091 3) 2201001200122)))
+
+(test-case
+  "Unit tests: from-k-ary"
+  (test-begin
+    (check-equal? (solutions.from-k-ary 0 7) 0)
+    (check-equal? (solutions.from-k-ary 8 9) 8)
+    (check-equal? (solutions.from-k-ary 2 3) 2)
+    (check-equal? (solutions.from-k-ary 12312312301 6) 86058829)
+    (check-equal? (solutions.from-k-ary 14321415 7) 1349913)
+    (check-equal? (solutions.from-k-ary 143213001 5) 757251)))
+
