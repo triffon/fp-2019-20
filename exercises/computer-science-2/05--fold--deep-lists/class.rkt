796cd2adf90453d7f9aad236953834dda4256c35
Deian Goranov <deian.goranov@gmail.com>
2019-11-04 12:02:18 +0200

    Add some examples and class template for week 5

---
+++ exercises/computer-science-2/05--fold--deep-lists/class.rkt
@@ -0,0 +1,62 @@
+; head, tail
+; eq, eqv, equal
+
+; list?
+
+; length
+; map
+; filter
+; append
+
+; foldr
+
+; чрез foldr
+; length
+; map
+; filter
+; append
+
+; foldl
+; foldl в Racket
+
+; minus-from
+; reverse 3
+
+; any? 2
+; all? 2
+; member? 2
+
+; remove
+; count-occurences
+
+;;;;;;;;;;;;;
+
+; take
+; drop
+
+
+; foldr1
+; foldl1
+; maximum
+ 
+; selection-sort
+; slice
+; zip
+; zip-with
+
+; unique
+; set operations
+
+; chunk
+
+;;;;;;;;;;;;;
+
+; atom?
+; count-atoms
+; flatten
+; deep-reverse
+; 
+; deep-foldr
+; deep-foldl
+; 
+; var-func
