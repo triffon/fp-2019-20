f9117c7a0dfc18d3c60ccff5d3f8df9d7c8a7fb5
Georgi Lyubenov <godzbanebane@gmail.com>
2019-11-10 20:10:11 +0200

    More TODOs

--- exercises/lab/exercises/06-start-undecided.hs
+++ exercises/lab/exercises/06-start-undecided.hs
@@ -1,2 +1,3 @@
 -- TODO: everyone is using inexact values for hw00, why?
 -- TODO: ping about homework 01, maybe show it? start it early!
+-- TODO: motivate to PR as soon as possible so we I can comment earlier
