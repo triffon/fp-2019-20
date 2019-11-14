0b3cb51ce813eb9aec5f835d1ea0721cb2fc8ac0
Georgi Lyubenov <godzbanebane@gmail.com>
2019-11-14 12:51:35 +0200

    WIP exercise 06

--- exercises/lab/exercises/06-start-undecided.hs
+++ exercises/lab/exercises/06-start-undecided.hs
@@ -1,5 +1,72 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-} -- cover all cases!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}      -- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}  -- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}      -- use different names!
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-} -- warn about incomplete patterns v2
+
 -- TODO: everyone is using inexact values for hw00, why?
 -- TODO: ping about homework 01, maybe show it? start it early!
 -- TODO: motivate to PR as soon as possible so we I can comment earlier
 -- TODO: ask people to NOT merge themselves
+
+-- TODO: talk about haskell philosophy (TYPESSSSSSSSSSSSSSSS, datatypes, laziness)
 -- TODO: tell people about resources
+-- TODO: say about NO FUKEN TABS IN MY HASKELL
+-- TODO: say about editor
+--
+-- TODO: show ghci
+-- it's a repl, we can evaluate
+-- :t(ype)
+-- :i(nfo)
+-- :l(oad)
+-- :r(eload)
+-- TODO: show types (NO IMPLICIT CASTING!!!)
+-- Int, Integer, Bool, Char, String, (a,b) (fst/snd), [a] (head/tail), etc
+-- undefined -- will use to fill stuff in sometimes
+-- TODO: show laziness? (vs scheme maybe?)
+-- TODO: type variables
+-- a -> a
+-- a -> b
+-- TODO: show syntax (shell syntax!)
+-- priority on fn application; infix!
+-- brackets for priority
+--
+-- TODO: declarations (datatypes - DATA) vs definitions (functions - OPERATIONS)
+-- pattern matching? say about warn incomplete/unused -- case?
+-- wildcards
+-- show examples? (programming languages?)
+-- show bool
+-- show pattern matching on bool for not and or?
+-- maybe show how pattern matching relates to laziness
+myIf :: Bool -> a -> a -> a
+myIf = undefined
+
+-- HOLES (example with tuple swap?)
+
+-- write nats and lists
+-- nats + lists again? (deriving show!)
+-- bitvectors?
+--
+--
+--
+--
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
+-- MAKE TASKKSKSKKSKSK
