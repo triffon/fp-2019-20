27ab7770e9fcba8c71c6cd40b7932a68f6d975e6
Georgi Lyubenov <godzbanebane@gmail.com>
2019-11-21 11:09:26 +0200

    Align all comments on pragmas..

--- exercises/lab/exercises/06-end-data.hs
+++ exercises/lab/exercises/06-end-data.hs
@@ -1,7 +1,7 @@
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-} -- cover all cases!
-{-# OPTIONS_GHC -fwarn-unused-matches #-}      -- use all your pattern matches!
-{-# OPTIONS_GHC -fwarn-missing-signatures #-}  -- write all your toplevel signatures!
-{-# OPTIONS_GHC -fwarn-name-shadowing #-}      -- use different names!
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}     -- cover all cases!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}          -- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}      -- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}          -- use different names!
 {-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-} -- warn about incomplete patterns v2
 
 -- We will now be studying Haskell.
