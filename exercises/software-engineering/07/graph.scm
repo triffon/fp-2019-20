9a8f11648bd79bb9b7f940fc86f9587aaf88889c
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2019-11-26 12:21:59 +0200

    Add graph constructors and transformations

--- exercises/software-engineering/07/graph.scm
+++ exercises/software-engineering/07/graph.scm
@@ -1,5 +1,11 @@
 (load "./association-list.scm")
 
+(define empty-graph '())
+
+(define (make-graph vs)
+  (make-alist (lambda (_) '())
+              vs))
+
 (define vertices keys)
 (define (children v g)
   (cdr (assoc v g)))
@@ -16,3 +22,26 @@
 
 (define (search-child v p g)
   (any? p (children v g)))
+
+(define (add-vertex v g)
+  (if (assoc v g)
+      g
+      (add-assoc v '() g)))
+
+(define (add-if-missing x l)
+  (if (member x l)
+      l
+      (cons x l)))
+
+(define (add-edge u v g)
+  (let ((g-with-u-v (add-vertex v (add-vertex u g))))
+    (add-assoc u
+               (add-if-missing v (children u g-with-u-v))
+               g-with-u-v)))
+
+(define (remove-edge u v g)
+  (add-assoc u
+             (filter (lambda (u)
+                       (not (equal? u v)))
+                     (children u g))
+             g))
