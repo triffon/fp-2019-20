5b297f905697ce8ba0549a3da5d1bd268c829928
Georgi Lyubenov <godzbanebane@gmail.com>
2019-11-28 17:50:22 +0200

    Move stuff which might go to next exercise

--- exercises/lab/exercises/08-undecided.hs
+++ exercises/lab/exercises/08-undecided.hs
@@ -178,34 +178,3 @@ quickSort = undefined
 group :: [Integer] -> [[Integer]]
 group = undefined
 
-
--- TODO: Maybe not below here?
-from :: Integer -> [Integer]
-from = undefined
-
-naturals :: [Integer]
-naturals = undefined
-
-facts :: [Integer]
-facts = undefined
-
-factsFaster :: [Integer]
-factsFaster = undefined
-
-fibs :: [Integer]
-fibs = undefined
-
-fibsFaster :: [Integer]
-fibsFaster = undefined
-
-prime :: Integer -> Bool
-prime = undefined
-
-primes :: [Integer]
-primes = undefined
-
-primesFaster :: [Integer]
-primesFaster = undefined
-
-eratosthenes :: [Integer] -> [Integer]
-eratosthenes = undefined
--- exercises/lab/exercises/09-undecided.hs
+++ exercises/lab/exercises/09-undecided.hs
@@ -1,2 +1,31 @@
---- TODO: Maybe?
---- TODO: Tree?
+-- TODO: Maybe?
+-- TODO: Tree?
+from :: Integer -> [Integer]
+from = undefined
+
+naturals :: [Integer]
+naturals = undefined
+
+facts :: [Integer]
+facts = undefined
+
+factsFaster :: [Integer]
+factsFaster = undefined
+
+fibs :: [Integer]
+fibs = undefined
+
+fibsFaster :: [Integer]
+fibsFaster = undefined
+
+prime :: Integer -> Bool
+prime = undefined
+
+primes :: [Integer]
+primes = undefined
+
+primesFaster :: [Integer]
+primesFaster = undefined
+
+eratosthenes :: [Integer] -> [Integer]
+eratosthenes = undefined
