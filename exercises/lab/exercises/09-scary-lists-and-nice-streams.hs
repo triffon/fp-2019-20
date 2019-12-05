a22a87543416d661c4398b110914136d7d712e4d
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-05 18:46:09 +0200

    Add warnings

--- exercises/lab/exercises/09-scary-lists-and-nice-streams.hs
+++ exercises/lab/exercises/09-scary-lists-and-nice-streams.hs
@@ -1,3 +1,8 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}     -- cover all cases!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}          -- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}      -- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}          -- use different names!
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-} -- warn about incomplete patterns v2
 -- TODO: hoogle
 -- TODO: foldl vs foldr, structural recursion, fold paper?
 
