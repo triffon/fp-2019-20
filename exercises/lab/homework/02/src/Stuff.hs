98dab6a9cf060a874cf053e214206e519f9199dc
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-10 15:52:53 +0200

    Add skeleton for tasks

--- exercises/lab/homework/02/src/Stuff.hs
+++ exercises/lab/homework/02/src/Stuff.hs
@@ -1,1 +1,36 @@
-module Stuff where
+module Stuff
+  ( group
+  , sortBy
+  , groupBy
+  , sortOn
+  , groupOn
+  , classifyOn
+  ) where
+
+group :: Eq a => [a] -> [[a]]
+group = undefined
+
+insertBy :: (a -> a -> Ordering) -> a -> [a] -> [a]
+insertBy = undefined
+
+sortBy :: (a -> a -> Ordering) -> [a] -> [a]
+sortBy = undefined
+
+-- Bonus: use NonEmpty inside
+groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy = undefined
+
+on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+on = undefined
+
+sortOn :: Ord b => (a -> b) -> [a] -> [a]
+sortOn = undefined
+
+groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
+groupOn = undefined
+
+(&&&) :: (a -> b) -> (a -> c) -> a -> (b, c)
+(&&&) = undefined
+
+classifyOn :: Ord b => (a -> b) -> [a] -> [[a]]
+classifyOn = undefined
