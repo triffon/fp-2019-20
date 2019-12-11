d99adb3634ab12f6c2c2330d23397346024b3699
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-11 09:55:53 +0200

    Add tests for NonEmpty

--- exercises/lab/homework/02/test/Main.hs
+++ exercises/lab/homework/02/test/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Main where
+module Main
+  ( main
+  ) where
 
-import Stuff
+import Stuff hiding (on, (&&&))
+import StuffNonEmpty
 
 import Data.List (sort)
 import Data.Function (on)
@@ -12,6 +15,11 @@ import Test.Hspec.QuickCheck
 
 main :: IO ()
 main = hspec do
+  stuffSpec
+  stuffNonEmptySpec
+
+stuffSpec :: Spec
+stuffSpec = do
   groupSpec
   sortBySpec
   groupBySpec
@@ -129,11 +137,87 @@ classifyOnSpec = describe "classifyOn" do
   withTransform (`div` 13) "(`div` 13)"
   withTransform (`mod` 42) "(`mod` 42)"
 
+stuffNonEmptySpec :: Spec
+stuffNonEmptySpec = do
+  groupNonEmptySpec
+  groupByNonEmptySpec
+  groupOnNonEmptySpec
+  classifyOnNonEmptySpec
+
+groupNonEmptySpec :: Spec
+groupNonEmptySpec = describe "groupNonEmpty" do
+  prop "Concatting after groupNonEmptying should yield the original list"
+    \(xs :: [Int]) -> concatNonEmpty (groupNonEmpty xs) == xs
+  prop "groupNonEmptys should contain only equal elements"
+    \(xs :: [Int]) -> allEqByNonEmpty (==) $ groupNonEmpty xs
+
+groupByNonEmptySpec :: Spec
+groupByNonEmptySpec = describe "groupByNonEmpty" do
+  let withPred eq eqName = describeWithFun eqName do
+        prop
+          do "Concatting after grouping should yield the original list"
+          \(xs :: [Int]) -> concatNonEmpty (groupByNonEmpty eq xs) == xs
+        prop
+          do "Groups should contain only equal elements"
+          \(xs :: [Int]) -> allEqByNonEmpty eq $ groupByNonEmpty eq xs
+
+  withPred (==) "default equality"
+  withPred ((==) `on` even) "evenness"
+  withPred ((==) `on` odd) "oddness"
+  withPred ((==) `on` (`div` 13)) "equality (`div` 13)"
+  withPred ((==) `on` (`mod` 42)) "equality (`mod` 42)"
+
+groupOnNonEmptySpec :: Spec
+groupOnNonEmptySpec = describe "groupOnNonEmpty" do
+  let withTransform :: Eq b => (Int -> b) -> String -> Spec
+      withTransform f fName = describeWithFun fName do
+        prop
+          do "Concatting after grouping should yield the original list"
+          \(xs :: [Int]) -> concatNonEmpty (groupOnNonEmpty f xs) == xs
+        prop
+          do "Groups should contain only equal elements"
+          \(xs :: [Int]) -> allEqByNonEmpty (==) $ map (mapNonEmpty f) $ groupOnNonEmpty f xs
+
+  withTransform id "id"
+  withTransform even "even"
+  withTransform odd "odd"
+  withTransform (`div` 13) "(`div` 13)"
+  withTransform (`mod` 42) "(`mod` 42)"
+
+classifyOnNonEmptySpec :: Spec
+classifyOnNonEmptySpec = describe "classifyOn" do
+  let withTransform :: Ord b => (Int -> b) -> String -> Spec
+      withTransform f fName = describeWithFun fName do
+        prop
+          do "Classification preserves the elements in the list"
+          \(xs :: [Int]) -> xs `sortEq` concatNonEmpty (classifyOnNonEmpty f xs)
+        prop
+          do "Classification groups contain only equal elements"
+          \(xs :: [Int]) -> allEqByNonEmpty (==) $ map (mapNonEmpty f) $ classifyOnNonEmpty f xs
+
+  withTransform id "id"
+  withTransform even "even"
+  withTransform odd "odd"
+  withTransform (`div` 13) "(`div` 13)"
+  withTransform (`mod` 42) "(`mod` 42)"
+
 allEqBy :: (a -> a -> Bool) -> [[a]] -> Bool
 allEqBy eq = all (\ys -> let y' = head ys in all (eq y') ys)
+
+allEqByNonEmpty :: (a -> a -> Bool) -> [NonEmpty a] -> Bool
+allEqByNonEmpty eq = all (\ys -> let (y':|_) = ys in allNonEmpty (eq y') ys)
+
+allNonEmpty :: (a -> Bool) -> NonEmpty a -> Bool
+allNonEmpty p (x:|xs) = p x && all p xs
 
 sortEq :: Ord a => [a] -> [a] -> Bool
 sortEq = (==) `on` sort
 
 describeWithFun :: String -> Spec -> Spec
 describeWithFun fName = describe ("for " ++ fName ++ ":")
+
+concatNonEmpty :: [NonEmpty a] -> [a]
+concatNonEmpty = concatMap toList
+
+toList :: NonEmpty a -> [a]
+toList (x:|xs) = x:xs
