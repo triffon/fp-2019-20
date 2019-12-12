9274333803e04a657fa7706df4d8c8550b3ddad4
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-12 17:10:20 +0200

    Add some stuff to next exercise

--- exercises/lab/exercises/11-undecided.hs
+++ exercises/lab/exercises/11-undecided.hs
@@ -1,3 +1,20 @@
+-- TODO: newtypes, records
+-- TODO: Any, All, Add, Mult, bitvectors, Endo?
 -- TODO: Maybe?
 -- TODO: Tree?
 
+-- foldmap with this :P
+--data Merge a = Merge [a]
+--
+--getMerge :: Merge a -> [a]
+--getMerge (Merge xs) = xs
+--
+--instance Ord a => Monoid (Merge a) where
+--  zero = []
+--  Merge x <> Merge y = Merge $ merge x y
+--    where
+--      merge [] ys = ys
+--      merge xs [] = xs
+--      merge (x:xs) (y:ys)
+--        | x <= y = x : merge xs (y:ys)
+--        | otherwise = y : merge (x:xs) ys
