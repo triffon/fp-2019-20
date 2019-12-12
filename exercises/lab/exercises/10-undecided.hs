d9519b6674f56813be69b98b34f55fedee3b345a
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-12 12:40:28 +0200

    Add some sketch of stuff for today

--- exercises/lab/exercises/10-undecided.hs
+++ exercises/lab/exercises/10-undecided.hs
@@ -1,2 +1,7 @@
 -- TODO: if two functions are nearly the same factor out the common bits!!
 -- TODO: project deadlines
+--
+-- TODO: newtypes
+-- TODO: TYPE CLASS ES - eq ((), Bool, nat, bv) ord ((), Bool, nat, bv??!?!),
+-- TODO: semigroup, monoid ????? maybe "PLUSSY" hehe
+-- TODO: Any, All, Add, Mult, bitvectors, Endo
