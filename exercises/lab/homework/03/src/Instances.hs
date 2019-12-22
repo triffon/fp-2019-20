a131eedfd17febb1bfcfab0404344c22e9e3e925
Georgi Lyubenov <godzbanebane@gmail.com>
2019-12-23 00:08:51 +0200

    Add instances tasks + tests

---
+++ exercises/lab/homework/03/src/Instances.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Instances where
+
+import Data.Char (isSpace)
+import Data.Function (on)
+
+newtype Pointwise a b = Pointwise {getPointwise :: (a, b)}
+  deriving (Show, Eq)
+
+instance (Ord a, Ord b) => Ord (Pointwise a b) where
+  (<=) :: Pointwise a b -> Pointwise a b -> Bool
+  (<=) = undefined
+
+newtype Lexicographic a b = Lexicographic {getLexicographic :: (a, b)}
+  deriving (Show, Eq)
+
+-- The default instance for tuples and lists
+instance (Ord a, Ord b) => Ord (Lexicographic a b) where
+  (<=) :: Lexicographic a b -> Lexicographic a b -> Bool
+  (<=) = undefined
+
+newtype Fun a b = Fun {getFun :: a -> b}
+
+instance (Semigroup b) => Semigroup (Fun a b) where
+  (<>) :: Fun a b -> Fun a b -> Fun a b
+  (<>) = undefined
+
+instance (Monoid b) => Monoid (Fun a b) where
+  mempty :: Fun a b
+  mempty = undefined
+
+newtype First a = First {getFirst :: Maybe a}
+  deriving (Eq, Show)
+
+instance Semigroup (First a) where
+  (<>) :: First a -> First a -> First a
+  (<>) = undefined
+
+instance Monoid (First a) where
+  mempty :: First a
+  mempty = undefined
+
+newtype Last a = Last {getLast :: Maybe a}
+  deriving (Eq, Show)
+
+instance Semigroup (Last a) where
+  (<>) :: Last a -> Last a -> Last a
+  (<>) = undefined
+
+instance Monoid (Last a) where
+  mempty :: Last a
+  mempty = undefined
+
+newtype Pair a b = Pair {getPair :: (a, b)}
+  deriving (Show, Eq)
+
+-- The default instance for tuples
+instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
+  (<>) :: Pair a b -> Pair a b -> Pair a b
+  (<>) = undefined
+
+instance (Monoid a, Monoid b) => Monoid (Pair a b) where
+  mempty :: Pair a b
+  mempty = undefined
+
+newtype Dual a = Dual {getDual :: a}
+  deriving (Show, Eq)
+
+instance Semigroup a => Semigroup (Dual a) where
+  (<>) :: Dual a -> Dual a -> Dual a
+  (<>) = undefined
+
+instance Monoid a => Monoid (Dual a) where
+  mempty :: Dual a
+  mempty = undefined
+
+data Flux a = Flux
+  { sides :: Maybe (a, a)
+  , changes :: Int
+  }
+  deriving (Show, Eq)
+
+flux :: a -> Flux a
+flux x = Flux (Just (x, x)) 0
+
+instance (Eq a) => Semigroup (Flux a) where
+  (<>) :: Flux a -> Flux a -> Flux a
+  (<>) = undefined
+
+instance (Eq a) => Monoid (Flux a) where
+  mempty :: Flux a
+  mempty = undefined
