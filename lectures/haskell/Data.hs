dc0cf84f1571e341aef0bf075eda3b3dd73a3791
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-01-08 10:47:15 +0200

    Примери за дефиниране на класове

---
+++ lectures/haskell/Data.hs
@@ -0,0 +1,40 @@
+module Data where
+
+type MathFun = Double -> Double
+type UnaryFunction a = a -> a
+type Dictionary k v = [(k,v)]
+
+class Measurable a where
+  size :: a -> Int
+  empty :: a -> Bool
+  empty = (==0) . size
+
+larger :: Measurable a => a -> a -> Bool
+larger x y = size x > size y
+
+instance Measurable Integer where
+  size 0 = 0
+  size n = 1 + size (n `div` 10)
+
+instance (Measurable a, Measurable b) => Measurable (a,b) where
+  size (x,y) = size x + size y
+
+instance Measurable a => Measurable [a] where
+  size = sum . map size
+
+data Weekday = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+today :: Weekday
+today = Wed
+isWeekend Sat = True
+isWeekend Sun = True
+isWeekend _   = False
+
+type Name = String
+type Score = Int
+data Player = Player Name Score
+katniss :: Player
+katniss = Player "Katniss Everdeen" 45
+
+better (Player name1 score1) (Player name2 score2)
+ | score1 > score2 = name1
+ | otherwise       = name2
