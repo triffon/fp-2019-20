c72760ee766bd392ac460a8f1a60a3945bd08234
dexterlb <dexterlb@qtrp.org>
2020-01-13 13:25:33 +0200

    final states in tries

--- exercises/informatics-3/06/trie.hs
+++ exercises/informatics-3/06/trie.hs
@@ -1,14 +1,15 @@
-data Trie = Node [Edge]
+data Trie = Node Finality [Edge]
 
+data Finality = Final | NonFinal
 type Edge = (Char, Trie)
 type Indent = String
 
 dicToTrie :: [String] -> Trie
-dicToTrie = foldr addToTrie (Node [])
+dicToTrie = foldr addToTrie (Node NonFinal [])
 
 addToTrie :: String -> Trie -> Trie
-addToTrie "" t = t
-addToTrie (a:suffix) (Node edges) = Node (map (addToEdge a suffix) (addEdge a edges))
+addToTrie "" (Node _ edges) = Node Final edges
+addToTrie (a:suffix) (Node final edges) = Node final (map (addToEdge a suffix) (addEdge a edges))
 
 addToEdge :: Char -> String -> Edge -> Edge
 addToEdge a suffix (b, t)
@@ -18,7 +19,7 @@ addToEdge a suffix (b, t)
 addEdge :: Char -> [Edge] -> [Edge]
 addEdge a edges
     | any (\(b, t) -> a == b) edges = edges
-    | otherwise                     = (a, Node []):edges
+    | otherwise                     = (a, Node NonFinal []):edges
 
 -- printing
 
@@ -26,27 +27,35 @@ instance Show Trie where
     show = showTrie
 
 showTrie :: Trie -> String
-showTrie = concat . (map (++ "\n")) . showTrieLines
+showTrie = concat . (map (++ "\n")) . slashify . showTrieLines
 
 showTrieLines :: Trie -> [String]
-showTrieLines (Node edges) = concat $ map showEdgeLines edges
+showTrieLines (Node    Final    []) = ["-*"]
+showTrieLines (Node NonFinal    []) = ["-."]
+showTrieLines (Node    Final edges) = concat $ map (showEdgeLines "-*-") edges
+showTrieLines (Node NonFinal edges) = concat $ map (showEdgeLines "-.-") edges
 
-showEdgeLines :: Edge -> [String]
-showEdgeLines (a, t) = showEdgeWithSuffix a (showTrieLines t)
+showEdgeLines :: String -> Edge -> [String]
+showEdgeLines marker (a, t) = showEdgeWithSuffix marker a (showTrieLines t)
 
-showEdgeWithSuffix :: Char -> [String] -> [String]
-showEdgeWithSuffix a [] = ["-" ++ [a]]
-showEdgeWithSuffix a (x:xs) = ("-" ++ [a] ++ "-" ++ x) : (map (("  "++) . slash) xs)
+showEdgeWithSuffix :: String -> Char -> [String] -> [String]
+showEdgeWithSuffix marker a [] = [marker ++ [a]]
+showEdgeWithSuffix marker a (x:xs) = (marker ++ [a] ++ "-" ++ x) : (map (("    "++) . slash) xs)
+
+slashify :: [String] -> [String]
+slashify [] = []
+slashify (s:ss) = s : (map ((""++) . slash) ss)
 
 slash :: String -> String
 slash "" = ""
-slash ('-':ss) = "\\-" ++ ss
+slash ('-':'.':ss) = " \\" ++ ss
+slash ('-':'*':ss) = " \\" ++ ss
 slash s = ' ':s
 
 -- demo
 
 myTrie :: Trie
-myTrie = dicToTrie ["foo", "bar", "baz", "вход", "извод", "изход"]
+myTrie = dicToTrie ["foo", "bar", "baz", "вход", "извод", "изход", "ba"]
 
 main = putStr $ show myTrie
 
