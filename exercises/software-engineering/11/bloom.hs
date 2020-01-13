af3d0e33588d5bb3785885247d35cb6e61a8d7ae
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-01-13 22:45:30 +0200

    Simplify bloom implementation

--- exercises/software-engineering/11/bloom.hs
+++ exercises/software-engineering/11/bloom.hs
@@ -2,6 +2,5 @@ import BinaryTree (BinaryTree(Empty, Node))
 
 bloom :: BinaryTree t -> BinaryTree t
 bloom Empty = Empty
-bloom (Node x Empty Empty) = Node x newLeaf newLeaf
-  where newLeaf = Node x Empty Empty
+bloom t@(Node x Empty Empty) = Node x t t
 bloom (Node x l r) = Node x (bloom l) (bloom r)
