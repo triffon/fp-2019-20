c60184e34cdd55e59e0f2b7462fb38ec2cff3df3
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-01-24 12:17:37 +0200

    Примери от гост-лекцията по ФП

--- lectures/haskell/FunctorsMonads.hs
+++ lectures/haskell/FunctorsMonads.hs
@@ -1,266 +1,167 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE InstanceSigs #-}
 module FunctorsMonads where
 
-import GHC.Base (returnIO)
+import Prelude hiding (Functor, fmap, (<$>),
+                       Applicative, pure, (<*>), Monad, (>>=))
 
-import Prelude hiding (Functor, fmap, (<$>), Applicative, pure, (<*>), sequenceA, (>>=), return, Monad)
+data Tree a = Empty | Leaf a | Tree { root :: a,
+                                      left :: Tree a,
+                                      right :: Tree a }
+                               deriving (Eq, Show)
 
-import Data (BinTree(..), mapBinTree, foldrBinTree, Tree(..), TreeList(..))
+type PairsList a b = [(a,b)]
 
-import Main (getInt)
+data Figure = Circle Float | Rectangle Float Float
 
-{-
-template <typename T>
-using List = LinkedList<T>
+instance Eq Figure where
+  Circle x == Circle y               = x == y
+  Rectangle w1 h1 == Rectangle w2 h2 = w1 == w2 && h1 == h2
+  _ == _                             = False
 
-List<T>
+-- type Player = (String, Int)
+-- data Player = Player String Int
+data Player = Player { name :: String, score :: Int}
+
+type StringOrInt = Either String Int
+f :: Int -> StringOrInt
+f x = if even x then Right x else Left "Error"
+
+g :: Int -> Maybe Int
+g x = if even x then Just x  else Nothing
+
+-- root x = x + 1
+
+{- !!!
+data MyEither a b = MyLeft { extract :: a } |
+                    MyRight { extract :: b }
 -}
 
-type AssocListDictionary k v = [(k,v)]
-type HashMapDictionary k v = [[(k,v)]]
-type BinTreeDictionary k v = BinTree (k,v)
-
-type Set d a = d a ()
 {-
-type AssocListSet a = Set AssocListDictionary a
-type HashSet a = Set HashMapDictionary a
-type BinTreeSet a = Set BinTreeDictionary a
+data NamedFigure = NamedCircle { fname :: String,
+                                 radius :: Float} |
+                   NamedRectangle { fname :: String,
+                               width :: Float,
+                               height :: Float }
 -}
--- TODO: class Dictionary
+
+data NamedFigure = NamedCircle String Float |
+                   NamedRectangle String Float Float
+
+fname :: NamedFigure -> String
+fname (NamedCircle name _) = name
+fname (NamedRectangle name _ _) = name
+
+data NamedThing a = NamedThing String a
+type NamedFigure2 = NamedThing Figure
 
 class Countable c where
-  count :: c a -> Integer
+  count :: c a -> Int
 
 instance Countable [] where
---  count :: [] a -> Integer
-  count :: [a] -> Integer
-  count = fromIntegral . length
-
-instance Countable Maybe where
-  count :: Maybe a -> Integer
-  count Nothing  = 0
-  count (Just _) = 1
-
-instance Countable BinTree where
-  count :: BinTree a -> Integer
-  count = foldrBinTree (+) 0 . mapBinTree (const 1)
-{-
-  count Empty = 0
-  count (Node x l r) = 1 + count l + count r
--}
-
-countTrees :: TreeList a -> Integer
-countTrees None           = 0
-countTrees (SubTree t ts) = count t + countTrees ts
+  count = length
 
 instance Countable Tree where
-  count :: Tree a -> Integer
-  count (Tree _ ts) = 1 + countTrees ts
+  count Empty = 0
+  count (Leaf _) = 1
+  count (Tree _ lt rt) = 1 + count lt + count rt
 
-{-
-instance Countable IO where
-  count x = 0
--}
+class Collectable c where
+  collect :: c a -> [a]
 
-class Listable c where
-  elements :: c a -> [a]
+instance Collectable [] where
+--  collect l = l
+  collect = id
 
-instance Listable [] where
---  elements :: [] a -> [a]
-  elements :: [a] -> [a]
-  elements = id
-
-instance Listable Maybe where
-  elements :: Maybe a -> [a]
-  elements Nothing = []
-  elements (Just x) = [x]
-
-instance Listable BinTree where
-  elements :: BinTree a -> [a]
-{-
-  elements Empty        = []
-  elements (Node x l r) = x : elements l ++ elements r
--}
-  elements = foldrBinTree (++) [] . mapBinTree (:[])
-
-elementsTrees :: TreeList a -> [a]
-elementsTrees None           = []
-elementsTrees (SubTree t ts) = elements t ++ elementsTrees ts
-
-instance Listable Tree where
-  elements :: Tree a -> [a]
-  elements (Tree x ts) = x : elementsTrees ts
+instance Collectable Tree where
+  collect Empty          = []
+  collect (Leaf x)       = [x]
+  collect (Tree x lt rt) = x : (collect lt ++ collect rt) -- КЛД
 
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
   (<$>) :: (a -> b) -> f a -> f b
   (<$>) = fmap
+  -- fmap id == id
 
 instance Functor [] where
-  fmap :: (a -> b) -> [a] -> [b]
   fmap = map
 
+instance Functor Tree where
+  fmap _ Empty           = Empty
+  fmap f (Leaf x)        = Leaf (f x)
+  fmap f (Tree x lt rt)  = Tree (f x) (fmap f lt) (fmap f rt)
+
 instance Functor Maybe where
-  fmap :: (a -> b) -> Maybe a -> Maybe b
   fmap _ Nothing  = Nothing
   fmap f (Just x) = Just (f x)
 
--- (a, b) = (,) a b
--- type PairInt a = (Int, a)
--- type PairInt a = (,) Int a
-type PairInt = (,) Int
-type PairBool = (,) Bool
-
-{-
-instance Functor PairInt where
-  fmap :: (a -> b) -> PairInt a -> PairInt b
-  fmap f (i, x) = (i, f x)
-
-instance Functor PairBool where
-  fmap :: (a -> b) -> PairBool a -> PairBool b
-  fmap f (i, x) = (i, f x)
--}
-
-instance Functor ((,) c) where
-  fmap :: (a -> b) -> (c, a) -> (c, b)
-  fmap f (i, x) = (i, f x)
-
--- TODO: направете (,) a c функтор, където c е фиксиран тип
-
--- Int -> Bool = (->) Int Bool
-
 instance Functor ((->) r) where
-  fmap :: (a -> b) -> (r -> a) -> (r -> b)
+--  g :: r -> a
+-- f == r -> ?
+--  fmap :: (a -> b) -> (f a) -> (f b)
+-- f a == r -> a
+-- f b == r -> b
+--  fmap :: (a -> b) -> (r -> a) -> (r -> b)
+--  fmap :: (a -> b) -> (r -> a) -> r -> b
+-- x :: r
+-- g :: r -> a
+-- f :: a -> b
+--  fmap f g x = f (g x)
+--  fmap f g = f . g
   fmap = (.)
-
-instance Functor (Either c) where
-  fmap :: (a -> b) -> Either c a -> Either c b
-  fmap _ (Left  x) = Left x
-  fmap f (Right y) = Right (f y)
-
--- TODO: Either a c е функтор с фиксирано c и вариращо a
-
-instance Functor BinTree where
-  fmap :: (a -> b) -> BinTree a -> BinTree b
-  fmap = mapBinTree
-
-fmapTrees :: (a -> b) -> TreeList a -> TreeList b
-fmapTrees _ None           = None
-fmapTrees f (SubTree t ts) = SubTree (fmap f t) (fmapTrees f ts)
-
-instance Functor Tree where
-  fmap :: (a -> b) -> Tree a -> Tree b
-  fmap f (Tree x ts) = Tree (f x) (fmapTrees f ts)
-
-instance Functor IO where
-  fmap :: (a -> b) -> IO a -> IO b
-  fmap f ioa = do x <- ioa
-                  returnIO (f x)
-
--- Just 3 :: Maybe Int
--- a = Int
--- b = Int -> Int
--- (+) :: Int -> (Int -> Int)
--- fmap (+) (Just 3) :: Maybe (Int -> Int)
 
 class Functor f => Applicative f where
   pure  :: a -> f a
   (<*>) :: f (a -> b) -> f a -> f b
-  -- fmap g x = (pure g) <*> x
+  -- fmap f x = pure f <*> x
   -- fmap = (<*>) . pure
 
 instance Applicative Maybe where
-  pure    = Just
+  pure = Just
+--  (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
   Nothing <*> _       = Nothing
   _       <*> Nothing = Nothing
-  Just f  <*> Just x  = Just $ f x
-
-instance Applicative (Either c) where
-  pure :: a -> Either c a
-  pure = Right
-  (<*>) :: Either c (a -> b) -> Either c a -> Either c b
-  Left z  <*> _       = Left z
-  _       <*> Left z  = Left z
-  Right f <*> Right x = Right (f x)
-
-{-
-!!!
-instance Applicative ((,) c) where
-  pure :: a -> (c, a)
-  pure x = (undefined, x)
--}
+  Just f  <*> Just x  = Just (f x)
 
 instance Applicative [] where
-  pure :: a -> [a]
-  pure = (:[])
-  (<*>) :: [a -> b] -> [a] -> [b]
-  --fs <*> xs = concat (map (\f -> map (\x -> f x) xs) fs)
-  -- fs <*> xs = concatMap (\f -> map (\x -> f x) xs) fs
-  -- fs <*> xs = concatMap (`map` xs) fs
+  pure x = [x]
+  -- (<*>) :: [a -> b] -> [a] -> [b]
   fs <*> xs = [ f x | f <- fs, x <- xs ]
 
-newtype ZipList a = ZipList { getZipList :: [a] }
-  deriving (Eq, Ord, Show, Read)
+data ZipList a = ZipList { getZipList :: [a] }
+  deriving (Eq, Ord, Show)
 
 instance Functor ZipList where
-  fmap :: (a -> b) -> ZipList a -> ZipList b
-  fmap f = ZipList . map f . getZipList
+  -- fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap f (ZipList xs) = ZipList (map f xs)
 
 instance Applicative ZipList where
-  pure :: a -> ZipList a
-  pure = ZipList . (:[])
-  (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
-  zfs <*> zxs = ZipList $ zipWith ($) (getZipList zfs) (getZipList zxs)
+  pure x = ZipList [x]
+  ZipList fs <*> ZipList xs =
+     ZipList [ f x | (f, x) <- zip fs xs ]
 
-instance Applicative ((->) r) where
-  pure :: a -> (r -> a)
-  pure = const
-  (<*>) :: (r -> a -> b) -> (r -> a) -> (r -> b)
-  (f <*> g) x = f x (g x)
-
-instance Applicative IO where
-  pure :: a -> IO a
-  pure = returnIO
-  (<*>) :: IO (a -> b) -> IO a -> IO b
-  iof <*> iox = do f <- iof
-                   x <- iox
-                   returnIO $ f x
-
-liftA2 :: (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
+liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 liftA2 f x y = f <$> x <*> y
 
-sequenceA :: (Applicative f) => [f a] -> f [a]
-sequenceA []     = pure []
-sequenceA (x:xs) = liftA2 (:) x $ sequenceA xs
-
-class (Applicative m) => Monad m where
+class Applicative m => Monad m where
   return :: a -> m a
   return = pure
 
   (>>=) :: m a -> (a -> m b) -> m b
   (>>)  :: m a -> m b -> m b
-  x >> y   =   x >>= \_ -> y
+  x >> y = x >>= (\z -> y)
 
 instance Monad Maybe where
-  Nothing >>= _  = Nothing
-  Just x  >>= f  = f x
+  return = Just
+  -- (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
+  Nothing  >>= _ = Nothing
+  (Just x) >>= f = f x
 
 instance Monad [] where
-  (>>=) :: [a] -> (a -> [b]) -> [b]
-  -- xs >>= f = concatMap f xs
-  (>>=) = flip concatMap
+--  return x = [x]
+--  return x = x:[]
+--  return x = (:[]) x
+  return = (:[])
 
-instance Monad ((->) r) where
-  (>>=) :: (r -> a) -> (a -> r -> b) -> r -> b
-  (f >>= g) x = g (f x) x
-
-getAt :: Integer -> [a] -> Maybe a
-getAt _ []  = Nothing
-getAt 0 (x:_) = Just x
-getAt n (_:xs) = getAt (n-1) xs
-
-instance Monad IO where
-  iox >>= f = do x <- iox
-                 f x
+-- (>>=) :: [a] -> (a -> [b]) -> [b]
+--  xs >>= f = concat (map f xs)
+  xs >>= f = concatMap f xs
